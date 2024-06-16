a70dee5213344822a456ac6af4ec95ef8c0d4d32
NurAnnisa01 <105841106021@student.unismuh.ac.id>
2024-06-16 17:05:03 +0800

    button

--- App.js
+++ App.js
@@ -1,37 +1,53 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Text, View, TextInput, Button } from "react-native";
+import React from "react";
 
 const App = () => {
+  const Inputan = ({ nama, color }) => {
+    return (
+      <TextInput
+        placeholder={`Masukan ${nama}`}
+        style={{
+          borderWidth: 1,
+          borderColor: "white",
+          borderRadius: 10,
+          width: 250,
+          height: 50,
+          marginVertical: 10,
+          padding: 10,
+          backgroundColor: "blue",
+          color: color,
+        }}
+      />
+    );
+  };
+
   return (
-
-    <View style = {{
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-      }}>
-    <View style={{
-      width:100,
-      height: 100,
-      backgroundColor: 'red',
-      alignSelf:'center'
-      }}></View>
-
-    <View style={{
-      width:100,
-      height: 100,
-      backgroundColor: 'blue',
-      alignSelf:'center'
-      
-      }}>
-
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-around",
+          width: "100%",
+        }}
+      >
+        <View style={{ alignItems: "center" }}>
+          <Text style={{ fontSize: 40, marginBottom: 150 }}></Text>
+          <Button title="Sign Up" onPress={() => {}} />
+        </View>
+        <View style={{ alignItems: "center" }}>
+          <Text style={{ fontSize: 40, marginBottom: 150}}></Text>
+          
+          <Button title="Sign In" onPress={() => {}} />
+        </View>
       </View>
-     
-
     </View>
+  );
+};
 
-  )
-}
-
-export default App
-
-const styles = StyleSheet.create({})
+export default App;
