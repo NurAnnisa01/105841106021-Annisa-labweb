62be182f5d84a77f68c7a5b066db6f4be99fe568
NurAnnisa01 <105841106021@student.unismuh.ac.id>
2024-06-28 11:08:56 +0800

    use font

--- App.js
+++ App.js
@@ -1,72 +1,41 @@
-import { StyleSheet, Text, View, TextInput } from "react-native";
-import React from "react";
-const ButtonCostum = ({ text, color }) => {
-  return (
-    <View>
-      <View
-        style={{
-          backgroundColor: color,
-          width: 250,
-          height: 100,
-          borderRadius: 50,
-          justifyContent: "center",
-          marginButtom: 10,
-        }}
-      >
-        <Text
-          style={{
-            textAlign: "center",
-            fontsize: 30,
-            fontWeight: "bold",
-          }}
-        >
-          {text}
-        </Text>
-      </View>
-    </View>
-  );
-};
+import { useFonts } from "expo-font";
+import { View, Text } from "react-native";
 
-// Custom component, TextInputCustom
-const TextInputCustom = ({ placeholder, color, typeKeyboard }) => {
-  return (
-    <TextInput
-      keyboardType={typeKeyboard}
-      placeholder={placeholder}
-      style={{
-        width: 250,
-        height: 50,
-        borderColor: color,
-        borderWidth: 3,
-        borderRadius: 10,
-        marginBottom: 10,
-        paddingLeft: 10,
-      }}
-    />
-  );
-};
-const App = () => {
+export default function App() {
+  const [dapatFont] = useFonts({
+    MetroBlack: require("./assets/fonts/Metropolis-Black.otf"),
+    MetroBold: require("./assets/fonts/Metropolis-Bold.otf"),
+    MetroLight: require("./assets/fonts/Metropolis-Light.otf"),
+    MetroSemiBold: require("./assets/fonts/Metropolis-SemiBold.otf"),
+  });
+
+  if (!dapatFont) {
+    return <Text>Font tidak ditemukan...</Text>;
+  }
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-      }}
-    >
-      <TextInputCustom
-        placeholder="Masukkan Nama"
-        color="pink"
-        typeKeyboard="default"
-      />
-      <TextInputCustom
-        placeholder="Masukkan Nomor hp"
-        color="pink"
-        typeKeyboard="numeric"
-      />
+      }}>
+      <Text
+        style={{
+          fontFamily: "MetroBlack",
+        }}>Font Metropolis Black</Text>
+      <Text
+        style={{
+          fontFamily: "MetroBold",
+        }}>Font Metropolis Semi Bold</Text>
+      <Text
+        style={{
+          fontFamily: "MetroLight",
+        }}> Font Metropolis Light</Text>
+      <Text
+        style={{
+          fontFamily: "MetroBold",
+        }} > Font Metropolis Bold</Text>
+      <Text> Ini Text biasa</Text>
     </View>
   );
-};
-export default App;
-
-const style = StyleSheet.create({});
+}
